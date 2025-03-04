3fee95f6cecc9633ec12def7629031081ac6bf5e
Sharon kosgei <jebetsharon089@gmail.com>
2025-03-04 06:53:56 +0300

    Pass variable declaration tests

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Declare the companyName variable as a constant with the value 'Scuber'
+const companyName = 'Scuber';
+
+// Declare mostProfitableNeighborhood as a let variable with the value 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Declare companyCeo as a let variable with the value 'Susan Smith'
+let companyCeo = 'Susan Smith';
